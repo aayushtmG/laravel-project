60de39c6a36c2915f29dffcf7949644554f5b5db
manoj karki <manojkkarki1234@gmail.com>
2025-01-21 14:57:34 +0545

    add localizaton word

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -4,6 +4,13 @@ return [
     'products' => 'Products',
     'about_us' => 'About Us',
     'contact' => 'Contact',
+    'message'=>'Message',
+    'services'=>'Services',
+    'team'=>'Team',
+    'news'=>'News',
+    'will appear'=>'will appear',
+    'here'=>'here',
+
 ];
 
 ?>
